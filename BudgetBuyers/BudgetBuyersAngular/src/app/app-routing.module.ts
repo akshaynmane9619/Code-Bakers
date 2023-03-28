d02c46d2a77b42b060673d34a8fef5fc31439262
Akshay Mane <1000071607@hexaware.com>
2023-03-28 13:04:43 +0530

    Hackathon project

---
+++ BudgetBuyers/BudgetBuyersAngular/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { LoginsuccessComponent } from './loginsuccess/loginsuccess.component';
+import { RegistrationComponent } from './registration/registration.component';
+
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'loginsuccess',component:LoginsuccessComponent},
+  {path:'registration',component:RegistrationComponent},
+  {path:'login',component:LoginComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ BudgetBuyers/BudgetBuyersAngular/src/app/app.module.ts
@@ -0,0 +1,37 @@
+
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginsuccessComponent } from './loginsuccess/loginsuccess.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    LoginsuccessComponent,
+    HomeComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
